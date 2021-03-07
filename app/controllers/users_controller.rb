f4f6ead2855e5de287e574c8cb9ba4fc56f91234
david <david.casper@yoco.com>
2021-03-07 20:41:21 +0200

    Adding methods back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-#   before_action :authorize, only: [:index, :me, :show]
-# before_action :set_user, only: [:show]
+  before_action :authorize, only: [:index, :me, :show]
+before_action :set_user, only: [:show]
 
   # GET /users
   def index
